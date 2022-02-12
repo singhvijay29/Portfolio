6665abfaa83c986f7855f8428473ad935b9f17f8
Vijay Kumar Singh <86338308+singhvijay29@users.noreply.github.com>
2022-02-12 12:14:17 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,7 +24,7 @@ export default function Projects() {
           Here are a few projects I've worked on recently.
           </p>
         </div>
-        <div className="flex flex-wrap -m-1 max-w-6xl justify-center" data-aos="zoom-out-down">
+        <div className="flex flex-wrap -m-1 max-w-7xl justify-center" data-aos="zoom-out-down">
           {projects.map((project) => (
             <a
               key={project.image}
@@ -32,7 +32,7 @@ export default function Projects() {
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-center overflow-hidden"
+                  className="absolute inset-0 w-full h-80 object-center overflow-hidden"
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
