248709213c48a9635877af87dedd79b1057baa93
singhvijay29 <singhvijay8529@gmail.com>
2024-06-02 13:22:50 +0530

    Added vercel analytics

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.render(
   <React.StrictMode>
-     <div class="cursor">
-    </div>
+    <div class="cursor"></div>
     <App />
+    <Analytics />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 var cursor = document.querySelector(".cursor");
-document.addEventListener('mousemove', e=>{
-    cursor.setAttribute("style", "top:" + (e.pageY -10) +"px; left:" + (e.pageX-10)+"px");
-    
-})  
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top:" + (e.pageY - 10) + "px; left:" + (e.pageX - 10) + "px"
+  );
+});
