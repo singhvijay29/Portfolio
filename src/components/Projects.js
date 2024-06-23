5afc12f809aae40ea25f13c1c2783eece1c05f84
Vijay Kumar Singh <86338308+singhvijay29@users.noreply.github.com>
2024-06-23 11:05:54 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -58,35 +58,38 @@ export default function Projects() {
             Here are a few projects I've worked on recently.
           </p>
         </div>
-        <div className="flex flex-wrap -m-1 max-w-7xl justify-center">
-          {projects.map((project) => (
-              <div key={project.image} className="flex relative sm:w-1/2 w-100 p-5"          
-                    ref={(el) => (cardRef.current[index] = el)}>
-                <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-80 object-center overflow-hidden"
-                  src={project.image}
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
-                  </h1>
-                  <p className="leading-relaxed">{project.description}</p>
-                  <div className="p-5 text-3xl">
-                    <a href={project.gitlink} className="mr-4">
-                      <i class="fab fa-github"></i>
-                    </a>
-                    <a href={project.link}>
-                      <i class="fas fa-link"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-          ))}
+          <div className="flex flex-wrap -m-1 max-w-7xl justify-center">
+      {projects.map((project, index) => (
+        <div
+          key={project.image}
+          className="flex relative sm:w-1/2 w-full p-5 card-container"
+          ref={(el) => (cardRef.current[index] = el)}
+        >
+          <img
+            alt="gallery"
+            className="absolute inset-0 w-full h-80 object-center overflow-hidden"
+            src={project.image}
+          />
+          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100 image-overlay">
+            <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+              {project.subtitle}
+            </h2>
+            <h1 className="title-font text-lg font-medium text-white mb-3">
+              {project.title}
+            </h1>
+            <p className="leading-relaxed">{project.description}</p>
+            <div className="p-5 text-3xl">
+              <a href={project.gitlink} className="mr-4">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href={project.link}>
+                <i className="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
         </div>
+      ))}
+    </div>
       </div>
     </section>
   );
