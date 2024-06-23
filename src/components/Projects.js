5d88651377d7332f2ec25654db803356550b1b26
Vijay Kumar Singh <86338308+singhvijay29@users.noreply.github.com>
2024-06-23 11:45:33 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,8 @@ import { projects } from "../data";
 
 export default function Projects() {
   return (
-    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+<div className="gradient-to-gray-900 w-[100vw] overflow-hidden">
+    <section id="projects" className="text-gray-400 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
@@ -50,6 +51,6 @@ export default function Projects() {
       </div>
 <br></br><br></br><br></br><br></br>      
     </section>
-    
+</div>
   );
 }
