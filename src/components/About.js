2e6cfc68be520dfb932f6ff85bb80a1e778bd434
Vijay Kumar Singh <86338308+singhvijay29@users.noreply.github.com>
2024-06-23 16:30:48 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,111 +2,98 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="gradient-to-gray-900 w-[100vw] overflow-hidden">
-    <section id="about" className="max-w-[920px] mx-auto">
+    <section id="about" className="max-w-[920px] mx-auto ">
       <div className="about-cont container mx-auto flex px-10 py-20 flex-col items-center">
-        <div className="flex flex-col lg:items-start lg:text-left mb-16 lg:mb-0 items-center text-center">
-          <h1 style={{ fontSize: "2em" }}>
+        <div className="flex flex-col lg:items-start text-left mb-16 lg:mb-0 items-center">
+          <h1 className="text-[28px] md:text-[38px] mb-6">
             LET ME <span className="purple"> INTRODUCE </span> MYSELF
           </h1>
-          <br></br>
-          <br></br>
-          <br></br>
           <div>
             <div>
               <div md={8} className="home-about-description">
-                <div
-                  className="home-about-body text-left"
-                  style={{ fontSize: "1.4em" }}
-                >
-                  <div className="font-semibold" style={{ fontSize: "1.6em" }}>
-                    {" "}
+                <div className="home-about-body text-left">
+                  <div className="font-semibold text-[24px] md:text-[32px]">
                     Frontend Engineer at Pazago Technologies
                   </div>
                   <br />
-                  As a passionate and innovative Frontend Engineer at Pazago
-                  Technologies, I specialize in creating seamless and engaging
-                  user experiences. With a strong foundation in web development
-                  and a keen eye for design, I collaborate closely with
-                  designers, product managers, and backend developers to deliver
-                  cutting-edge solutions that drive customer satisfaction and
-                  business success. <br />
-                  <br />
-                  <div
-                    className="font-semibold mb-5"
-                    style={{ fontSize: "1.3em" }}
-                  >
+                  <div className="text-[16px] md:text-[20px]">
+                    As a passionate and innovative Frontend Engineer at Pazago
+                    Technologies, I specialize in creating seamless and engaging
+                    user experiences. With a strong foundation in web
+                    development and a keen eye for design, I collaborate closely
+                    with designers, product managers, and backend developers to
+                    deliver cutting-edge solutions that drive customer
+                    satisfaction and business success. <br />
+                  </div>
+                  <div className="font-semibold mt-5 mb-3 text-[20px] md:text-[24px]">
                     Professional Highlights:
                   </div>
                   <div className="flex flex-col text-[24px] gap-4">
-                    <div>
+                    <div className="text-[16px] md:text-[20px]">
                       <span className="text-[#f2f29] font-[600]">
                         Innovative Web Development:
-                      </span>
+                      </span>{" "}
                       I revamped user experiences by developing interactive web
                       applications using Next.js, Tailwind CSS, and Chakra UI,
                       achieving a 50% increase in page load speed and a 20%
                       boost in user retention.
                     </div>
-                    <div>
+                    <div className="text-[16px] md:text-[20px]">
                       <span className="text-[#f2f29] font-[600]">
                         Cross-functional Collaboration:
-                      </span>
+                      </span>{" "}
                       Ensuring seamless integration of frontend and backend
                       systems through close collaboration with designers and
                       backend developers.
                     </div>
-                    <div>
+                    <div className="text-[16px] md:text-[20px]">
                       <span className="text-[#f2f29] font-[600]">
                         Performance Optimization:
-                      </span>
+                      </span>{" "}
                       Optimized website performance and ensured cross-browser
                       compatibility, significantly enhancing user experience and
                       search visibility.
                     </div>
-                    <div>
+                    <div className="text-[16px] md:text-[20px]">
                       <span className="text-[#f2f29] font-[600]">
                         Feature Development:
-                      </span>
+                      </span>{" "}
                       Contributed to key projects, such as implementing shipment
                       tracking with Google Maps API, developing real-time chat
                       with WebSocket technology, and enabling push notifications
                       for real-time updates.
                     </div>
-                    <div>
+                    <div className="text-[16px] md:text-[20px]">
                       <span className="text-[#f2f29] font-[600]">
                         Advanced Functionality:
-                      </span>
+                      </span>{" "}
                       Designed and implemented features like a dynamic text
                       editor, multi-select document functionality, CSV file
                       reader, and PDF document generator, all aimed at improving
                       usability and efficiency.
                     </div>
-                    <div>
+                    <div className="text-[16px] md:text-[20px]">
                       <span className="text-[#f2f29] font-[600]">
                         Technical Excellence:
-                      </span>
+                      </span>{" "}
                       Migrated JavaScript to TypeScript, resulting in 30% faster
                       development and 20% fewer bugs, and optimized website
                       performance with techniques like lazy loading and code
                       splitting.
                     </div>
-                    <div>
+                    <div className="text-[16px] md:text-[20px]">
                       <span className="text-[#f2f29] font-[600]">
                         User-centric Solutions:
-                      </span>
+                      </span>{" "}
                       Created a comprehensive Admin Dashboard with visually
                       captivating graphs and developed a modern wallet system
                       handling 1,000+ monthly transactions.
                     </div>
                   </div>
-                  <div
-                    className="font-semibold mb-5 mt-8"
-                    style={{ fontSize: "1.3em" }}
-                  >
+                  <div className="font-semibold mt-8 mb-3 text-[20px] md:text-[24px]">
                     Education:
                   </div>
-                  <div className="flex flex-col text-[24px] gap-4">
+                  <div className="flex flex-col text-[16px] md:text-[20px] gap-2">
                     <div>
                       Full Stack Web Development - Masai School, Bangalore
                     </div>
@@ -119,6 +106,5 @@ export default function About() {
         </div>
       </div>
     </section>
-</div>
   );
 }
