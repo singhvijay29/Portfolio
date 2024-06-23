79bd763e6f512c3818ef667980a31eaf0ff08052
Vijay Kumar Singh <86338308+singhvijay29@users.noreply.github.com>
2024-06-23 10:13:50 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,25 +9,53 @@ export default function Skills() {
       <br></br>
       <h2>Tech Stacks</h2>
       <div class="skills-div">
+      <div class="skills-card">
+          <img
+            src="https://cdn.svgporn.com/logos/nextjs-icon.svg"
+            alt="nextjs"
+          />
+          <h3>Next.js</h3>
+        </div>
         <div class="skills-card">
           <img
             src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
-            alt=""
+            alt="react"
           />
           <h3>React</h3>
         </div>
         <div class="skills-card">
           <img
-            src="https://cdn.freebiesupply.com/logos/large/2x/nodejs-icon-logo-png-transparent.png"
-            alt=""
+            src="https://cdn.svgporn.com/logos/react-query-icon.svg"
+            alt="react-query"
           />
-          <h3>Node</h3>
+          <h3>React Query</h3>
+        </div>
+        <div class="skills-card">
+          <img src="https://cdn.svgporn.com/logos/redux.svg" alt="" />
+          <h3>Redux</h3>
         </div>
         <div class="skills-card">
           <img src="https://cdn.svgporn.com/logos/javascript.svg" alt="" />
           <h3>JavaScript</h3>
         </div>
-
+        <div class="skills-card">
+          <img
+            src="https://cdn.svgporn.com/logos/typescript-icon.svg"
+            alt="typescript"
+          />
+          <h3>Typescript</h3>
+        </div>
+        <div class="skills-card">
+          <img
+            src="https://cdn.svgporn.com/logos/websocket.svg"
+            alt="websocket"
+          />
+          <h3>Websocket</h3>
+        </div>
+        <div class="skills-card">
+          <img src="https://cdn.svgporn.com/logos/tailwindcss-icon.svg" alt="tailwindcss" />
+          <h3>Tailwindcss</h3>
+        </div>
         <div class="skills-card">
           <img src="https://cdn.svgporn.com/logos/css-3.svg" alt="" />
           <h3>CSS</h3>
@@ -41,28 +69,38 @@ export default function Skills() {
         </div>
         <div class="skills-card">
           <img
+            src="https://cdn.svgporn.com/logos/flutter.svg"
+            alt="flutter"
+          />
+          <h3>Flutter</h3>
+        </div>
+        <div class="skills-card">
+          <img
+            src="https://cdn.svgporn.com/logos/dart.svg"
+            alt="dart"
+          />
+          <h3>Dart</h3>
+        </div>
+        <div class="skills-card">
+          <img
             src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
-            alt=""
+            alt="git"
           />
           <h3>Git</h3>
         </div>
         <div class="skills-card">
-          <img src="https://cdn.svgporn.com/logos/redux.svg" alt="" />
-          <h3>Redux</h3>
+          <img
+            src="https://cdn.svgporn.com/logos/webpack.svg"
+            alt="webpack"
+          />
+          <h3>Webpack</h3>
         </div>
         <div class="skills-card">
           <img
-            src="https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_original_wordmark_logo_icon_146425.png"
+            src="https://cdn.freebiesupply.com/logos/large/2x/nodejs-icon-logo-png-transparent.png"
             alt=""
           />
-          <h3>MongoDB</h3>
-        </div>
-        <div class="skills-card">
-          <img
-            src="https://miro.medium.com/max/512/1*fVBL9mtLJmHIH6YpU7WvHQ.png"
-            alt=""
-          />
-          <h3>Postman</h3>
+          <h3>Node</h3>
         </div>
       </div>
       <br></br>
