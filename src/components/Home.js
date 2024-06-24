112a0b7336c4e08e355dd10090da64440df01712
Vijay Kumar Singh <86338308+singhvijay29@users.noreply.github.com>
2024-06-24 22:23:35 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -64,7 +64,7 @@ export default function Home() {
           </div>
         </div>
         <div
-          className="lg:max-w-lg lg:w-2/3 w-5/6 m-auto"
+          className="lg:max-h-[60vh] m-auto"
           data-aos="fade-in"
           data-aos-duration="700"
         >
